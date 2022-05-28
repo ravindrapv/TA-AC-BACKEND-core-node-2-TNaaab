07953d7d89bcda080f7d0495093f0b833c4b3d73
ravindrapv <ravindra.ravi4849@gmail.com>
2022-05-28 19:03:56 +0530

    server paths

---
+++ block-BNaaax/index.js
@@ -0,0 +1,8 @@
+
+const path = require("path");
+const indexPath = path.join(__dirname, "server.js");
+
+
+console.log(__dirname);
+console.log(__filename);
+console.log(indexPath);
--- block-BNaaax/server.js
+++
@@ -1,7 +0,0 @@
-
-const path = require("path");
-const indexPath = path.join(__dirname, "server.js");
-
-
-console.log(path);
-console.log(indexPath);
